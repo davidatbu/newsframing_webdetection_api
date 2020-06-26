eaad6bbdc607b8ade9786d2cb0d71a95ced915e8
davidatbu <davidat@bu.edu>
2020-06-26 12:20:31 -0400

    small bugfix.

--- processors.py
+++ processors.py
@@ -45,13 +45,13 @@ class FrameProcessor(MultiPhaseTrainProcessor):
     def get_dev_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_csv(os.path.join(data_dir, "dev.csv")), "dev"
+            self._read_csv(os.path.join(data_dir, "dev.tsv")), "dev"
         )
 
     def get_test_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_csv(os.path.join(data_dir, "test.csv")), "test"
+            self._read_csv(os.path.join(data_dir, "test.tsv")), "test"
         )
 
     def get_labels(self):
